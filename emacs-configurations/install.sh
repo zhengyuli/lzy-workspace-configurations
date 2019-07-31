77e70acc606f10ef412c5923325d656bf1e64351
lli <lli@splunk.com>
2019-07-31 11:48:47 +0800

    move install.sh into emacs-configurations directory

--- emacs-configurations/install.sh
+++ emacs-configurations/install.sh
@@ -2,9 +2,9 @@
 
 #---------------------------------------------------------------------------------
 # Name: install.sh
-# Purpose:
+# Purpose: Emacs configurations setup script
 #
-# Time-stamp: <2019-03-15 10:40:35 Friday by lli>
+# Time-stamp: <2019-07-31 11:46:38 Wednesday by lli>
 #
 # Author: zhengyu li
 # Created: 2014-03-26
@@ -21,8 +21,8 @@ BASE_DIR=$(cd $(dirname $0); pwd)
 EMACS_CONFIG_FILE=$HOME/.emacs
 
 echo "Setup emacs configurations ... .. ."
-cp -fv ${BASE_DIR}/emacs-configurations/init.el ${BASE_DIR}/.emacs.local
-sed -i -e s:_EMACS_CONFIG_ROOT_PATH_:${BASE_DIR}/emacs-configurations/:g ${BASE_DIR}/.emacs.local
+cp -fv ${BASE_DIR}/init.el ${BASE_DIR}/.emacs.local
+sed -i '' s:_EMACS_CONFIG_ROOT_PATH_:${BASE_DIR}/:g ${BASE_DIR}/.emacs.local
 if [ -e $EMACS_CONFIG_FILE ]; then
     rm $EMACS_CONFIG_FILE
 fi
