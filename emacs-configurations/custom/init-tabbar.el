f8cca8a65388864f8652fc0cb3487ca336846731
l00592207 <lizhengyu7@huawei.com>
2020-11-02 13:45:43 +0800

    minor update

--- emacs-configurations/custom/init-tabbar.el
+++ emacs-configurations/custom/init-tabbar.el
@@ -1,5 +1,5 @@
 ;;; package --- init-tabbar.el ---
-;; Time-stamp: <2018-12-10 18:44:03 Monday by lli>
+;; Time-stamp: <2020-11-02 13:44:06 Monday by lizhengyu>
 
 ;; Copyright (C) 2013 zhengyu li
 ;;
@@ -45,6 +45,50 @@
 (customize-set-variable 'tabbar-buffer-home-button '(("") ""))
 (customize-set-variable 'tabbar-scroll-left-button '(("") ""))
 (customize-set-variable 'tabbar-scroll-right-button '(("") ""))
+
+;; Customize `tabar-buffer-groups'
+(defun tabbar-buffer-groups ()
+  "Return the list of group names the current buffer belongs to.
+Return a list of one element based on major mode."
+  (list
+   (cond
+	((member (buffer-name)
+			 '("*scratch*" "*Messages*"))
+	 "Common"
+	 )
+	((eq major-mode 'dired-mode)
+	 "Dired"
+	 )
+	((memq major-mode
+		   '(help-mode apropos-mode Info-mode Man-mode))
+	 "Help"
+	 )
+	((memq major-mode
+		   '(rmail-mode
+			 rmail-edit-mode vm-summary-mode vm-mode mail-mode
+			 mh-letter-mode mh-show-mode mh-folder-mode
+			 gnus-summary-mode message-mode gnus-group-mode
+			 gnus-article-mode score-mode gnus-browse-killed-mode))
+	 "Mail"
+	 )
+	((eq major-mode 'term-mode)
+	 "Terminal")
+	((or (get-buffer-process (current-buffer))
+		 ;; Check if the major mode derives from `comint-mode' or
+		 ;; `compilation-mode'.
+		 (tabbar-buffer-mode-derived-p
+		  major-mode '(comint-mode compilation-mode)))
+	 "Process"
+	 )
+	(t
+	 ;; Return `mode-name' if not blank, `major-mode' otherwise.
+	 (if (and (stringp mode-name)
+			  ;; Take care of preserving the match-data because this
+			  ;; function is called when updating the header line.
+			  (save-match-data (string-match "[^ ]" mode-name)))
+		 mode-name
+	   (symbol-name major-mode))
+	 ))))
 
 ;; ==================================================================================
 ;; Key bindings for `tabbar'
